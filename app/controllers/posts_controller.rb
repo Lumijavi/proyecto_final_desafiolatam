9487afe234b86284d13a05356a6820c261daa617
lumijavi <basturn@gmail.com>
2020-01-24 19:55:32 -0300

    added kaminari to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.order("created_at DESC").page(params[:page])
   end
 
   # GET /posts/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   mount_uploader :photo, PhotoUploader
 
+  paginates_per 6
 end
